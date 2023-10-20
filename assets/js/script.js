6e26a824fb287b0b8137e4adfc584e706cea668c
Kieran Hillman <developedbykieran@gmail.com>
2023-10-20 13:41:58 +0000

    Add variables for DOM elements, add click function for search button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
-console.log("SCRIPT DEPLOYED");
-
 const apiKey = "f3250885431f58878f508fbb70480058";
+
+// DOM elements
+const cityInput = document.getElementById('city');
+const searchBtn = document.getElementById('search-btn');
+const weatherInfo = document.getElementById('weather-info');
+
+// Add click event to search button
+searchBtn.addEventListener('click', async () => {
+    const city = cityInput.value;
+    const coordinates = await fetchCoordinates(city);
+    fetchWeather(coordinates.lat, coordinates.lon);
+})
